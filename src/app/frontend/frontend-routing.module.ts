824d16544190629a7363278b21ff96a4185e501f
Ankit Suthar <anksuthar@gmail.com>
2020-01-10 22:48:10 +0530

    Frontend login component add

--- src/app/frontend/frontend-routing.module.ts
+++ src/app/frontend/frontend-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FrontendComponent } from './frontend.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { 
     path: '', component: FrontendComponent,
-    children: [{path: 'home', component: HomeComponent}]
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent }
+    ]
   }
 ];
 
--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FrontendComponent } from './frontend.component';
 import { HomeComponent } from './home/home.component';
 import { FrontendRoutingModule } from './frontend-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     FrontendComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     FrontendRoutingModule
