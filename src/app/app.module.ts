0bcce31d0bf6b154aae5bc7971900aac053e85f4
Ankit Suthar <anksuthar@gmail.com>
2020-01-11 11:11:29 +0530

    Install angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FrontendModule } from './frontend/frontend.module'
+import { FrontendModule } from './frontend/frontend.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { FrontendModule } from './frontend/frontend.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FrontendModule
+    FrontendModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
